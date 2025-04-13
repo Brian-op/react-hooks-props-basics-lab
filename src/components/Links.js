1319fe158e5eacdfc51768e49c02eaca62733676
Brian <brian.kiprono8@student.moringaschool.com>
2025-04-13 07:10:34 +0300

    rendering props

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Links = () => {
+  return (
+    <div>Links
+      <div>
+        github={"https://github.com/liza"}
+        linkedin={"https://www.linkedin.com/in/liza/"}
+      </div>
+    </div>
+  )
+}
+
+export default Links
